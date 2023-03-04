aad3ca3dba8aa4863b784112eea5450570541c9b
Leonardo <lbvargas99@gmail.com>
2023-03-04 18:56:57 -0300

    feat: adding spring project

---
+++ src/main/java/com/lb/wscars/WscarsApplication.java
@@ -0,0 +1,13 @@
+package com.lb.wscars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WscarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WscarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lb/wscars/WscarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lb.wscars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WscarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
